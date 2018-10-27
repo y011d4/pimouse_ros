4fc9840ba8a8966b33ef633d48fddddf0b72881f
Yo Iida <yoiida333@gmail.com>
2018-10-27 07:06:45 +0000

    fix motor calculation

--- scripts/motors1.py
+++ scripts/motors1.py
@@ -45,7 +45,7 @@ class Motor():
         self.set_raw_freq(message.left_hz, message.right_hz)
 
     def callback_cmd_vel(self, message):
-        forward_hz = 8000.0*message.linear.x/(9*math.pi)
+        forward_hz = 80000.0*message.linear.x/(9*math.pi)
         rot_hz = 400.0*message.angular.z/math.pi
         self.set_raw_freq(forward_hz-rot_hz, forward_hz+rot_hz)
         self.using_cmd_vel = True
