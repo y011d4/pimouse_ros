f45bee5abbc5115a74d0a1d786d80a9bd3d01f8f
Yo Iida <yoiida333@gmail.com>
2018-10-27 06:53:53 +0000

    try test

--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -23,9 +23,11 @@ class MotorTest(unittest.TestCase):
         m = MotorFreqs()
         m.left_hz = 123
         m.right_hz = 456
+        self.assertEqual(m.left_hz, 123, "nazono error")
         for i in range(10):
             pub.publish(m)
             time.sleep(0.1)
+        time.sleep(3)
 
         self.file_check("rtmotor_raw_l0", m.left_hz, "wrong left value from motor_raw")
         self.file_check("rtmotor_raw_r0", m.right_hz, "wrong right value from motor_raw")
